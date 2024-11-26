3c990fe5a90568a802e47ca9d850dd6a7ce22038
jessiacb <106203043+jessiacb@users.noreply.github.com>
2024-11-26 11:41:53 -0500

    fonts

--- pokemon-trainer-hub/client/src/pages/Home.js
+++ pokemon-trainer-hub/client/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./Home.css"; // Make sure to include the CSS file
 
 const Home = () => (
-  <div className="text-center">
-    <h1>Welcome to the Pokémon Trainer Hub!</h1>
+  <div className="home-container">
+    <h1 className="pokemon-title">Welcome to the Pokémon Trainer Hub!</h1>
     <p>Start building your Pokémon teams and explore the world of Pokémon.</p>
   </div>
 );
+
 export default Home;
 
